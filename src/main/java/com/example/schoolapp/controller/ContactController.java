f1b6f7a5149a27b7f153bf4092267e09066f8637
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-03 12:35:31 +0530

    Set up Holidays page

--- src/main/java/com/example/schoolapp/controller/ContactController.java
+++ src/main/java/com/example/schoolapp/controller/ContactController.java
@@ -2,8 +2,6 @@ package com.example.schoolapp.controller;
 
 import com.example.schoolapp.model.Contact;
 import com.example.schoolapp.service.ContactService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +12,6 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ContactController {
-
-    private final Logger log = LoggerFactory.getLogger(ContactController.class);
 
     private final ContactService contactService;
 
