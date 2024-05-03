78930b42a74caff2e43a151cf06e75faf11953ba
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-03 14:20:08 +0530

    Use @Slf4j Lombok annotation for logging purpose

--- src/main/java/com/example/schoolapp/service/ContactService.java
+++ src/main/java/com/example/schoolapp/service/ContactService.java
@@ -1,19 +1,17 @@
 package com.example.schoolapp.service;
 
 import com.example.schoolapp.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 
+@Slf4j
 @Service
 public class ContactService {
 
-    private static final Logger log = LoggerFactory.getLogger(ContactService.class);
-
     public void saveMessageDetails(Contact contact) {
-
         log.info(contact.toString());
-
     }
 }
