ed435b78f3051107f203eade8c0a35451ee48a17
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-04 21:38:36 +0530

    Implement login, dashboard page along with the functionality

--- src/main/java/com/example/schoolapp/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/schoolapp/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.example.schoolapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -40,5 +43,22 @@ public class ProjectSecurityConfig {
         http.httpBasic(withDefaults());
 
         return http.build();
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
