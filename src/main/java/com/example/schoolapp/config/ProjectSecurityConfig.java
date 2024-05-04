53bd5612027b181e9f304e707a288af5f894f396
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-04 16:44:22 +0530

    Configure all paths to be publicly accessible by using permitAll()

---
+++ src/main/java/com/example/schoolapp/config/ProjectSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.schoolapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/","/home").permitAll()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+        );
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
