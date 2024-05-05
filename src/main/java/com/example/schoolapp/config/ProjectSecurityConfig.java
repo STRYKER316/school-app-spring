6547d0ffb03b562bea6c272be8711b43131e2810
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-05 17:17:05 +0530

    Set up configurations for connecting to H2 DB

--- src/main/java/com/example/schoolapp/config/ProjectSecurityConfig.java
+++ src/main/java/com/example/schoolapp/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.schoolapp.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,7 +19,8 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg"))
+        http.csrf((csrf) -> csrf.ignoringRequestMatchers("/saveMsg")
+                                .ignoringRequestMatchers(PathRequest.toH2Console()))
                 .authorizeHttpRequests((requests) -> requests.requestMatchers("/", "/home").permitAll()
                 .requestMatchers("/dashboard").authenticated()
                 .requestMatchers("/login").permitAll()
@@ -27,6 +31,7 @@ public class ProjectSecurityConfig {
                 .requestMatchers("/courses").permitAll()
                 .requestMatchers("/about").permitAll()
                 .requestMatchers("/assets/**").permitAll()
+                .requestMatchers(PathRequest.toH2Console()).permitAll()
         );
 
         http.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login")
@@ -42,6 +47,8 @@ public class ProjectSecurityConfig {
         );
 
         http.httpBasic(withDefaults());
+
+        http.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
 
         return http.build();
     }
@@ -62,4 +69,45 @@ public class ProjectSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
     }
+
+
+//    @Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+//
+//        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+//
+//        http.csrf((csrf) -> csrf.ignoringRequestMatchers(mvcMatcherBuilder.pattern("/saveMsg"))
+//                        .ignoringRequestMatchers(PathRequest.toH2Console()))
+//                .authorizeHttpRequests((requests) -> requests.requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/home")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/dashboard")).authenticated()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/login")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/logout")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/holidays/**")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/contact")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/saveMsg")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/courses")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/about")).permitAll()
+//                        .requestMatchers(mvcMatcherBuilder.pattern("/assets/**")).permitAll()
+//                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+//                );
+//
+//        http.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login")
+//                .defaultSuccessUrl("/dashboard")
+//                .failureUrl("/login?error=true")
+//                .permitAll()
+//        );
+//
+//        http.logout(httpSecurityLogoutConfigurer -> httpSecurityLogoutConfigurer.logoutUrl("/logout")
+//                .logoutSuccessUrl("/login?logout=true")
+//                .invalidateHttpSession(true)
+//                .permitAll()
+//        );
+//
+//        http.httpBasic(withDefaults());
+//
+//        http.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
+//
+//        return http.build();
+//    }
 }
