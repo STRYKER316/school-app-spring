44ab15ab4a929997d425e00706b510ae2f1a1b3b
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-05 21:58:04 +0530

    Implement functionality to fetch Holidays list from DB rather than showing hardcoded list

--- src/main/java/com/example/schoolapp/controller/HolidaysController.java
+++ src/main/java/com/example/schoolapp/controller/HolidaysController.java
@@ -1,6 +1,7 @@
 package com.example.schoolapp.controller;
 
 import com.example.schoolapp.model.Holiday;
+import com.example.schoolapp.repository.HolidaysRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,33 +15,11 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidaysController {
+    private final HolidaysRepository holidaysRepository;
 
-////    Handle Query Parameters
-//    @GetMapping("/holidays")
-//    public String displayHolidays(@RequestParam(required = false) boolean festival,
-//                                  @RequestParam(required = false) boolean federal, Model model) {
-//
-//        model.addAttribute("festival", festival);
-//        model.addAttribute("federal", federal);
-//
-//        List<Holiday> holidays = Arrays.asList(
-//                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-//                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-//                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-//                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-//                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-//                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-//                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-//                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-//        );
-//
-//        Holiday.Type[] types = Holiday.Type.values();
-//        for (Holiday.Type type : types) {
-//            model.addAttribute(type.toString(),
-//                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
-//        }
-//        return "holidays";
-//    }
+    public HolidaysController(HolidaysRepository holidaysRepository) {
+        this.holidaysRepository = holidaysRepository;
+    }
 
 
 //    Handle Path Variables
@@ -56,16 +35,7 @@ public class HolidaysController {
             model.addAttribute("festival",true);
         }
 
-        List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
-        );
+        List<Holiday> holidays = holidaysRepository.findAllHolidays();
 
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
