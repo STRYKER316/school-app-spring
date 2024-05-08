1c30dffd0ac6a4921a541ee2ad0deb5f60a0c2a6
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-08 14:48:32 +0530

    Create Person, Address, Roles models and their repositories

---
+++ src/main/java/com/example/schoolapp/repository/RolesRepository.java
@@ -0,0 +1,10 @@
+package com.example.schoolapp.repository;
+
+import com.example.schoolapp.model.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Roles, Integer> {
+
+}
