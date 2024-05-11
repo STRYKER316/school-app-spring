8c8ee9d8b6d924bc8f1be70b388c00d649d2153c
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-11 15:20:16 +0530

    Create class UsernamePwdAuthProvider as an implementation for AuthenticationProvider interface

---
+++ src/main/java/com/example/schoolapp/security/UsernamePwdAuthProvider.java
@@ -0,0 +1,55 @@
+package com.example.schoolapp.security;
+
+import com.example.schoolapp.model.Person;
+import com.example.schoolapp.model.Roles;
+import com.example.schoolapp.repository.PersonRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class UsernamePwdAuthProvider implements AuthenticationProvider {
+
+    private final PersonRepository personRepository;
+
+    public UsernamePwdAuthProvider(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        Person person = personRepository.getByEmail(email);
+
+        if (person != null && person.getPersonId() > 0 && password.equals(person.getPwd())) {
+            return new UsernamePasswordAuthenticationToken(person.getName(), password, getGrantedAuthorities(person.getRole()));
+        } else {
+            throw new BadCredentialsException("Invalid Credentials");
+        }
+    }
+
+
+    private List<GrantedAuthority> getGrantedAuthorities(Roles role) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
+        return grantedAuthorities;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
