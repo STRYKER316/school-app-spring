afbd23b74a0b4d9c9881c333bcb378bbc9fd9d66
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-16 20:20:52 +0530

    Make API resource paths unavailable in HAL Explorer using @RepositoryRestResource annotation

--- src/main/java/com/example/schoolapp/repository/CourseRepository.java
+++ src/main/java/com/example/schoolapp/repository/CourseRepository.java
@@ -2,10 +2,12 @@ package com.example.schoolapp.repository;
 
 import com.example.schoolapp.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "courses")
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
--- src/main/java/com/example/schoolapp/repository/RolesRepository.java
+++ src/main/java/com/example/schoolapp/repository/RolesRepository.java
@@ -2,8 +2,10 @@ package com.example.schoolapp.repository;
 
 import com.example.schoolapp.model.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource(exported = false)
 @Repository
 public interface RolesRepository extends JpaRepository<Roles, Integer> {
 
