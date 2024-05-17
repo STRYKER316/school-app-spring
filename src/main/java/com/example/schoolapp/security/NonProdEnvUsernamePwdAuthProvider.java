0f690c2f8e67c0b3959c939d62a23e8faeeb0164
Anshil Mishra <ansil.mishra316@gmail.com>
2024-05-17 15:25:33 +0530

    Use the PasswordAuth bean conditionally using @Profile depending on the environment

--- src/main/java/com/example/schoolapp/security/NonProdEnvUsernamePwdAuthProvider.java
+++ src/main/java/com/example/schoolapp/security/NonProdEnvUsernamePwdAuthProvider.java
@@ -3,6 +3,7 @@ package com.example.schoolapp.security;
 import com.example.schoolapp.model.Person;
 import com.example.schoolapp.model.Roles;
 import com.example.schoolapp.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -17,41 +18,35 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Component
-@Profile("prod")
-public class UsernamePwdAuthProvider implements AuthenticationProvider {
+@Profile("!prod")
+public class NonProdEnvUsernamePwdAuthProvider implements AuthenticationProvider {
 
     private final PersonRepository personRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public UsernamePwdAuthProvider(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
+    @Autowired
+    public NonProdEnvUsernamePwdAuthProvider(PersonRepository personRepository) {
         this.personRepository = personRepository;
-        this.passwordEncoder = passwordEncoder;
     }
-
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String email = authentication.getName();
-        String password = authentication.getCredentials().toString();
-
         Person person = personRepository.findByEmail(email);
 
-        if (person != null && person.getPersonId() > 0 && passwordEncoder.matches(password, person.getPwd())) {
-            return new UsernamePasswordAuthenticationToken(email, password, getGrantedAuthorities(person.getRole()));
+        if( null != person && person.getPersonId() > 0) {
+            return new UsernamePasswordAuthenticationToken(email, null, getGrantedAuthorities(person.getRole()));
         } else {
-            throw new BadCredentialsException("Invalid Credentials");
+            throw new BadCredentialsException("Invalid credentials!");
         }
     }
 
-
-    private List<GrantedAuthority> getGrantedAuthorities(Roles role) {
+    private List<GrantedAuthority> getGrantedAuthorities (Roles roles) {
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_"+roles.getRoleName()));
+
         return grantedAuthorities;
     }
-
 
     @Override
     public boolean supports(Class<?> authentication) {
